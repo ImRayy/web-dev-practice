0612303bb1f9389167830e33d32021fd96fcc3b9
RAY <souradeep20roy01@gmail.com>
2023-12-19 23:09:52 +0530

    ✨ Added BentoGrid style-1

---
+++ src/utils/imageLinks.ts
@@ -0,0 +1,37 @@
+type ImageDataType = {
+  title: string;
+  subTitle: string;
+  image: string;
+};
+export const images: ImageDataType[] = [
+  {
+    title: "Aesthetic Globe",
+    subTitle: "Mailad Fakurian",
+    image:
+      "https://images.unsplash.com/photo-1618005198919-d3d4b5a92ead?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "3 dimensional wave",
+    subTitle: "Simon Lee",
+    image:
+      "https://images.unsplash.com/photo-1634017839464-5c339ebe3cb4?q=80&w=1035&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Pink Room",
+    subTitle: "Mo Eid",
+    image:
+      "https://images.pexels.com/photos/8657160/pexels-photo-8657160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    title: "Rainbow eggs",
+    subTitle: "Towfiqu barbhuiya",
+    image:
+      "https://images.pexels.com/photos/11287865/pexels-photo-11287865.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    title: "Mei Kusakabe From Totoro",
+    subTitle: "Afif Ramdhasuma",
+    image:
+      "https://images.unsplash.com/photo-1630207831419-3532bcb828d7?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];

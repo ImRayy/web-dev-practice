e0ee09e1d2105f00037e93b3731b0717697b3fa8
RAY <souradeep20roy01@gmail.com>
2024-01-03 01:16:03 +0530

    🎨 Updated homepage and structure

---
+++ src/utils/projectLinks.ts
@@ -0,0 +1,17 @@
+export const projects = [
+  {
+    url: "landingpage/style-1",
+    thumbnail:
+      "https://ik.imagekit.io/rayshold/FrontendProjects/landingpage/style-1.webp?updatedAt=1704221916525",
+  },
+  {
+    url: "auth-form/style-1",
+    thumbnail:
+      "https://ik.imagekit.io/rayshold/FrontendProjects/auth-form/style-1.webp?updatedAt=1704221858211",
+  },
+  {
+    url: "grid/style-1",
+    thumbnail:
+      "https://ik.imagekit.io/rayshold/FrontendProjects/grid/style-1.webp?updatedAt=1704221886904",
+  },
+];
